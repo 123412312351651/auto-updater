1dae7105ae62a17ce038f3c85cc25ddb0c9e16ba
123412312351651 <leog34900@gmail.com>
2025-10-09 16:43:25 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -26,11 +26,6 @@ while FileName do
     FileName = findNextFile(Handle)
 end
 
-function
-    if not isSampLoaded() or not isSampfuncsLoaded() then return end
-    while not isSampAvailable() do wait(100) end
-end
-
 function sampev.onServerMessage(color, message)
     if message:find("@555") then
         sampSendChat("123456")
